36a52b6146d3fcc01fecbaec3d6e4dc687130712
Your Name <vijaeswar@gmail.com>
2021-08-01 20:52:01 +0530

    firstcommit

---
+++ day3/src/day3/Hypo.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class Hypo {
+		static double hypo(double a,double b)
+		{
+			double c=(float)Math.pow((a*a+b*b), 0.5);
+			return c;
+		}
+
+		public static void main(String[] args) {
+
+			System.out.println((int)hypo(5,4));
+			System.out.println(hypo(34.5,23.6));
+
+		}
+
+	}
+
---
+++ day3/src/day3/Mass_of_earth.java
@@ -0,0 +1,10 @@
+package day3;
+
+public class Mass_of_earth  {
+
+		public static void main(String[] args) {
+			double moe=5.9728*Math.pow(10, 24);
+			System.out.println(moe);
+		}
+	}
+
